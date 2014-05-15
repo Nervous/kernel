0424d9fd6737b15cd4f333c5804e5fea7522f5ce
Xavier Gerondeau <xavier.gerondeau@epita.fr>
2014-05-15 20:00:24 +0200

    Fix gdt.h

--- k/gdt.c
+++ k/gdt.c
@@ -1,8 +1,17 @@
 #include "gdt.h"
 
+s_gdt_entry[6] gdt;
+
+void set_entry(t_uint32 base, t_uint16 limit, t_uint8 type, int nb)
+{
+  s_gdt_entry tmp;
+  st.limit_low = limit
+}
+
 void set_gdt()
 {
-  s_gdt_entry[6] gdt; 
   memset(gdt, 0, sizeof(s_gdt_entry) * 6);
 
 }
+
+
