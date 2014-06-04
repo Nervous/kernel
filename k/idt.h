cb07ffdf0a417fd6531e414d9964026ef3d0dd12
Xavier Gerondeau <xavier.gerondeau@epita.fr>
2014-06-04 14:12:09 +0200

    Start idt

--- k/idt.h
+++ k/idt.h
@@ -1,4 +1,19 @@
 #ifndef IDT_HH
 # define IDT_HH
     //void flud_idt(STRUCT_IDT_ADDR idt_ptr);
-#endif
+
+
+struct idtdesc {
+  u16 offset0_15;
+  u16 select;
+  u16 type;
+  u16 offset16_31;
+} __attribute__ ((__packed__));
+
+/* Registre IDTR */
+struct idtr {
+  u16 limite;
+  u32 base;
+} __attribute__ ((__packed__));
+
+#endif/* Descripteur de segment */
