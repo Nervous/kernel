30768f02cd06fc1cffe64246a1772c0f5a339c70
Xavier Gerondeau <xavier.gerondeau@epita.fr>
2014-06-05 15:34:24 +0200

    Fix errcode

--- k/idt_func.c
+++ k/idt_func.c
@@ -4,105 +4,96 @@
 
 void default_int(void)
 {
-    printf("%s", "interrupt\n");
+    printf("%s", "interrupt\r\n");
 }
 
 void handler_0(void)
 {
-    printf("%s", "Divide Error");
-    put_err_code();
+    printf("%s", "Divide Error\r\n");
     int_ret();
 }
 
 void handler_1(void)
 {
-    printf("%s", "Reserved");
-    put_err_code();
+    printf("%s", "Reserved\r\n");
     int_ret();
 }
 
 void handler_2(void)
 {
-    printf("%s", "NMI Interrupt");
-    put_err_code();
+    printf("%s", "NMI Interrupt\r\n");
     int_ret();
 }
 
 void handler_3(void)
 {
-    printf("%s", "Breakpoint");
-    put_err_code();
+    printf("%s", "Breakpoint\r\n");
     int_ret();
 }
 
 void handler_4(void)
 {
-    printf("%s", "Overflow");
-    put_err_code();
+    printf("%s", "Overflow\r\n");
     int_ret();
 }
 
 void handler_5(void)
 {
-    printf("%s", "Bound Range Exceeded");
-    put_err_code();
+    printf("%s", "Bound Range Exceeded\r\n");
     int_ret();
 }
 
 void handler_6(void)
 {
-    printf("%s", "Invalid Opcode");
-    put_err_code();
+    printf("%s", "Invalid Opcode\r\n");
     int_ret();
 }
 
 void handler_7(void)
 {
-    printf("%s", "Devide not Available");
-    put_err_code();
+    printf("%s", "Devide not Available\r\n");
+    
     int_ret();
 }
 
 void handler_8(void)
 {
-    printf("%s", "Double Fault");
-    int_ret();
+    printf("%s", "Double Fault\r\n");
+    int_ret_err();
 }
 
 void handler_9(void)
 {
-    printf("%s", "Coprocessor Segment Overrun");
-    put_err_code();
+    printf("%s", "Coprocessor Segment Overrun\r\n");
     int_ret();
 }
 
 void handler_10(void)
 {
-    printf("%s", "Invalid TSS");
-    int_ret();
+    printf("%s", "Invalid TSS\r\n");
+    int_ret_err();
 }
 
 void handler_11(void)
 {
-    printf("%s", "Segment Not Present");
-    int_ret();
+    printf("%s", "Segment Not Present\r\n");
+    int_ret_err();
 }
 
 void handler_12(void)
 {
-    printf("%s", "Stack-Segment Fault");
-    int_ret();
+    printf("%s", "Stack-Segment Fault\r\n");
+    int_ret_err();
 }
 
 void handler_13(void)
 {
-    printf("%s %d", "General Protection dasdas", get_err_code());
-    put_err_code();
-    int_ret();
+    printf("%s", "General Protection\r\n");
+    int_ret_err();
 }
 
 void handler_14(void)
 {
-    printf("%s", "PageFault");
-    int_ret();
+    printf("%s", "PageFault\r\n");
+    int_ret_err();
 }
